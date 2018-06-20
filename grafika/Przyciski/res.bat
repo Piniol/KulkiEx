f4766b5b1c36b199816c5b8f570b3401fd10e638
Piniol <wiechec.przemyslaw@gmail.com>
2018-06-20 15:40:35 +0200

    Add files via upload

---
+++ grafika/Przyciski/res.bat
@@ -0,0 +1,5 @@
+@echo off
+del przyciski.res
+D:
+cd delphi2005\bin
+brcc32 D:\KulkiEx15\grafika\Przyciski\przyciski.rc
---
+++ grafika/res.bat
@@ -0,0 +1,5 @@
+@echo off
+del plansza.res
+D:
+cd delphi2005\bin
+brcc32 D:\KulkiEx15\grafika\plansza.rc
